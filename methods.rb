853f1c5f2dbd911afb99518471504bca4d966248
Kirthi Nandi <kirthi.nandi@gmail.com>
2022-08-29 09:45:44 -0700

    commit tree d589c15d262ece57cec0122f088f7673c912fbd9

--- methods.rb
+++ methods.rb
@@ -1,1 +1,28 @@
 # Your code here!
+
+def greet_programmer
+  puts "Hello, programmer!"
+end
+
+def greet(name) 
+  puts "Hello, #{name}!"
+end
+
+greet("Naureen")
+
+def greet_with_default(name = "programmer") 
+  puts "Hello, #{name}!"
+end
+
+greet_with_default("Naureen")
+
+def add(num1, num2) 
+  return num1 + num2
+end
+
+def halve(number) 
+  return nil unless number.class == Integer
+
+  number / 2
+end
+
